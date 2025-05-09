394daa903c6bcec6318c345b9eb846b551daa7d0
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-05-09 09:50:30 -0400

    Add feature to connect to multiple walls at once

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -14,6 +14,7 @@ namespace DWallSelector
     {
         private ObservableCollection<string> _dWallList = new ObservableCollection<string>();
         private const string _fileName = "dwalls.json";
+        private int _connectedWalls = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -57,38 +58,46 @@ namespace DWallSelector
 
         public void OnConnectBtnClick(object sender, RoutedEventArgs e)
         {
-            if(DWallsList.SelectedItem is string selectedIP)
+            foreach(var dWall in DWallsList.SelectedItems)
             {
-                Console.WriteLine($"Connecting to {selectedIP}...");
-                ConnectBtn.IsEnabled = false;
-                DWallsList.IsEnabled = false;
-
-                var process = new Process
+                if (dWall is string selectedIP)
                 {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "cmd.exe",
-                        Arguments = $"/k telnet {selectedIP}",
-                        UseShellExecute = true
-                    },
-                    EnableRaisingEvents = true
-                };
+                    Console.WriteLine($"Connecting to {selectedIP}...");
+                    ConnectBtn.IsEnabled = false;
+                    DWallsList.IsEnabled = false;
 
-                process.Exited += (s, args) =>
+                    var process = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            Arguments = $"/k telnet {selectedIP}",
+                            UseShellExecute = true
+                        },
+                        EnableRaisingEvents = true
+                    };
+
+                    process.Exited += (s, args) =>
+                    {
+                        Dispatcher.UIThread.Post(() =>
+                        {
+                            _connectedWalls--;
+                            if (_connectedWalls == 0)
+                            {
+                                ConnectBtn.IsEnabled = true;
+                                DWallsList.IsEnabled = true;
+                                Console.WriteLine("Telnet window closed.");
+                            }
+                        });
+                    };
+
+                    process.Start();
+                    _connectedWalls++;
+                }
+                else
                 {
-                    Dispatcher.UIThread.Post(() =>
-                    {
-                        ConnectBtn.IsEnabled = true;
-                        DWallsList.IsEnabled = true;
-                        Console.WriteLine("Telnet window closed.");
-                    });
-                };
-
-                process.Start();
-            } 
-            else
-            {
-                Console.WriteLine("No IP selected");
+                    Console.WriteLine("No IP selected");
+                }
             }
         }
 
